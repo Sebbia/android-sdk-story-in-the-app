f91b3044b1a824243a75d811941a1d204372ddf4
Qweeno <daniag2001@yandex.ru>
2020-12-06 21:00:35 +0300

    Class
    -Добавлен класс истории
    -Добавлен класс экрана истории

---
+++ app/src/main/java/com/example/android_sdk_story_in_the_app/History.kt
@@ -0,0 +1,11 @@
+package com.example.android_sdk_story_in_the_app
+
+class History {
+
+    var id : Int = 0
+
+    lateinit var skin :String
+
+    lateinit var screenList : MutableList<Screen>
+
+}
---
+++ app/src/main/java/com/example/android_sdk_story_in_the_app/Screen.kt
@@ -0,0 +1,25 @@
+package com.example.android_sdk_story_in_the_app
+
+import java.util.*
+
+class Screen {
+
+    var token : Int = 0
+
+    var id : Int = 0
+
+    lateinit var skin : String
+
+    var time : Int = 0
+
+    lateinit var DateStart : Date
+
+    lateinit var DateEnd : Date
+
+    lateinit var title : String
+
+    lateinit var subtitle : String
+
+    lateinit var url : String
+
+}
